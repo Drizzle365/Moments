4df01829bec2075967f6ca9797f0e84e0c1e0906
Drizzle <i@drizzle.vip>
2023-06-04 19:12:37 +0800

    fix:调整js监听

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,18 +1,18 @@
 ﻿// 如果nav元素存在就执行下面的时间监听，不存在就不执行
 const navScroll = function () {
-  const nav = document.querySelector('.header-top');
-  const header = document.querySelector('.header');
-  if (window.scrollY > header.offsetHeight) {
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
+    const nav = document.querySelector('.header-top');
+    const header = document.querySelector('.header');
+    if (nav && header) {
+        if (window.scrollY > header.offsetHeight) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    }
 }
-if (document.querySelector('.header-top')) {
-  document.addEventListener('scroll', navScroll);
-} else {
-  document.removeEventListener('scroll', navScroll);
-}
+
+document.addEventListener('scroll', navScroll);
+
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
